00e26b76ced53a4a33f93aa26054c1925217f453
phathua <37536650+phathua@users.noreply.github.com>
2023-12-24 08:46:33 +0700

    Create hyper.is-an.app.js

---
+++ domains/hyper.is-an.app.js
@@ -0,0 +1,18 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: 'Share Account Web', // describe your project in this field
+  domain: '1bt.uk', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'hyper', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/phathua/free-domains',
+    email: 'phat2003qn',
+  },
+  record: {
+    CNAME: 'hyper.is-an.app', // e.g.: <your-github-account>.github.io
+    TXT: ['v=spf1 mx a include:mail0.serv00.com -all'],
+    A: ['128.204.218.48'],
+    NS: ['dns1.serv00.com', 'dns2.serv00.com'],
+  },
+  proxy: false
+})
